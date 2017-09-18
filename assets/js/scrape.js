28fb09de7c59b144f16f308c4578d711d595f388
Val Wong <valentin.wong@gmail.com>
2017-09-17 20:08:55 -0700

    fix append on scrape.js

--- assets/js/scrape.js
+++ assets/js/scrape.js
@@ -1,14 +1,31 @@
-$(document).on("click", "#scrape", function(){
+$(document).ready(function() {
+  console.log('loaded scrape.js');
+  $(document).on("click", "#scrape", function() {
 
-  $.getJSON("/scrape", function(data) {
-    // For each one
-    $("#articles").html('');
-    for (var i = 0; i < 10; i++) {
-      // Display the apropos information on the page
-      $("#articles").append("<p id= "+data[i].headline +">" + data[i].summary + "<br />" + data[i].link + "</p>");
-     $("#articles").append("<button class="+"save"+" id= "+ data[i].headline +">" +"SAVE"+ "</button>");
-    };
-//Save function
+    //   $.getJSON("/scrape", function(data) {
+    //     // For each one
+    //     $("#articles").html('');
+    //     for (var i = 0; i < 10; i++) {
+    //       // Display the apropos information on the page
+    //       $("#articles").append("<p id= "+data[i].headline +">" + data[i].summary + "<br />" + data[i].link + "</p>");
+    //      $("#articles").append("<button class="+"save"+" id= "+ data[i].headline +">" +"SAVE"+ "</button>");
+    //     };
+    // //Save function
+    //
+    //   }) ;
+    console.log('clicked');
 
-  }) ;
+    $.get("/allarticles", function(data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        var html = "<div class='articleStuff'>" +
+                  "<h2>" + data[i].title + "</h2>" +
+                   "<p>"  + data[i].summary + "</p>" + "</div>";
+        $("#articles").append(html);
+      };
+
+    });
+
+
+  });
 });
--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 // Requiring our Note and Article models
-// var Note = require("./models/Note.js");
-// var Article = require("./models/Article.js");
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
@@ -60,36 +60,57 @@ app.get("/", function(req, res) {
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
   // console.log("entered /scrape");
+  var result = {};
   request("https://www.nytimes.com/", function(error, response, html) {
     // console.log("entered request");
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(html);
-    var arr = [];
+    // var arr = [];
+
     // Now, we grab every h2 within an article tag, and do the following:
     $("article.story.theme-summary").each(function(i, element) {
       // console.log("entered loop");
       // Save an empty result object
-      var result = {};
+      // var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.headline = $(this).children("h2").text();
+      result.title = $(this).children("h2").text();
+      result.link = $(this).children("h2").children("a").attr("href");
       result.summary = $(this).children("p.summary").text().trim();
-      result.url = $(this).children("h2").children("a").attr("href");
+
       // result.link = $(this).children("a").attr("href");
-      if(result.headline && result.summary && result.url){
-        arr.push(result);
-      }
-      //save articles in db
-      if(result.summary){
-        arr.push(result);
+      if(result.title && result.summary && result.link){
+        // arr.push(result);
+        let newArticle = new Article(result);
+
+        newArticle.save(function(err, doc){
+          if(err){
+            console.log(err);
+          }
+          else{
+            console.log('saved stuff: ',doc);
+          }
+        });
+
       }
     });
-    res.json(arr);
+    res.redirect("/");
     // console.log(arr);
   });
   // Tell the browser that we finished scraping the text
   console.log("Scrape Complete");
 });
+
+app.get("/allarticles", function(req, res){
+  Article.find({}, function(err, articles) {
+  //do stuff
+  if(err){
+    console.log(err);
+  }else{
+    res.json(articles);
+  }
+  });
+})
 
 // Listen on port 3000
 app.listen(3000, function() {
